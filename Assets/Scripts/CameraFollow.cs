48f37c880fce4e37e9b41b43aa3f95d05658e326
alexandercstephens <alexandercstephens@gmail.com>
2015-12-12 14:47:42 -0600

    initial camera script

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+    public Transform toFollow;
+    Vector3 diff;
+	
+    void Awake () {
+        diff = transform.position - toFollow.position;
+    }
+
+	void Update () {
+        transform.position = toFollow.position + diff;
+	}
+}

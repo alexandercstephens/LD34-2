daac42d316e3efadf286238b49c3d750b8567ee5
Bill Cheng <cccheng118@yahoo.com>
2015-12-13 12:11:46 -0600

    Bird + Bird Poo
    Added Bird and Bird Poo

---
+++ Assets/Scripts/BirdAI.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BirdAI : MonoBehaviour
+{
+
+    public Vector2 Direction;
+    private Rigidbody2D rigidBody;
+    public Vector2 LiftingForce;
+    private float startY;
+    public LayerMask ToDetect;
+    public GameObject Poo;
+
+    // Use this for initialization
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        rigidBody.velocity = Direction;
+
+        startY = transform.position.y;
+    }
+
+    public void DoLiftingForce()
+    {
+        rigidBody.velocity.Set(rigidBody.velocity.x, 0);
+
+        var diffY = startY - transform.position.y;
+
+        LiftingForce.y = 15 + diffY * 20;
+        rigidBody.AddForce(LiftingForce);
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        // Start pooping
+        var poo =(GameObject) Instantiate(Poo, transform.position, Quaternion.identity);
+        var pooRigidBody = poo.GetComponent<Rigidbody2D>();
+        pooRigidBody.velocity = new Vector2(rigidBody.velocity.x, 0);
+    }
+
+}
---
+++ Assets/Scripts/BirdPoo.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BirdPoo : MonoBehaviour {
+
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Debug.Log("Hit");
+        var rigidBody = GetComponent<Rigidbody2D>();
+        rigidBody.velocity = Vector3.zero;
+        rigidBody.angularVelocity = 0;
+
+        Destroy(gameObject, 2f);
+    }
+
+}

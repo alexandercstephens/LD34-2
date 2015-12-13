381e0da74d40f7213fe5b0a7a72b2cdc9568c67b
alexandercstephens <alexandercstephens@gmail.com>
2015-12-12 19:14:05 -0600

    parallax and updated scene

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Parallax : MonoBehaviour {
+    public Transform parallaxee;
+
+    float origP;
+    float origC;
+    float diff;
+
+	void Awake () {
+        origP = parallaxee.position.x;
+        origC = transform.position.x;
+    }
+	
+	void Update () {
+        parallaxee.position = new Vector3(origP + 0.5f * (transform.position.x - origC), parallaxee.position.y, parallaxee.position.z);
+	}
+}

b899f05866b6502d04f771ae4cd82d2eaa9b2279
zerosauce <zerosauce@users.noreply.github.com>
2015-12-13 00:50:01 -0600

    Improved camera controls and made them simpler. Keeps up better with the character now.

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -3,77 +3,37 @@ using System.Collections;
 
 public class CameraFollow : MonoBehaviour
 {
-    [SerializeField]
-    Transform character;
+    public Transform Player;
 
-    [SerializeField]
-    private float playerVelocity = 0.0f;
+    public Vector2 Smoothing, Margin;
     
+    public bool IsFollowing { get; set; }
 
-    [SerializeField]
-    float speed = 6;
-
-    [SerializeField]
-    float characterX;
-
-    [SerializeField]
-    float characterY;
-
-    [SerializeField]
-    float movementThresholdX = 3;
-
-    [SerializeField]
-    float movementThresholdY = 2;
-
-    private Vector3 movementToPosition;
-
-    [SerializeField]
-    public float smoothTime = 1.5f;
-    
-    //camera variables
-    private float cameraSizeInput;
-    private float cameraSizeChanger;
-    public float smoothTimeCameraSize = 0.5f;
-    private Vector3 cameraVelocity = Vector3.zero;
-
-    private float getCharacterPositionRelativeToCamera(float characterPos, float cameraPos)
+    public void Start()
     {
-        if (characterPos > cameraPos)
-            return characterPos - cameraPos;
-        else
-            return cameraPos - characterPos;
+        IsFollowing = true;
+        Smoothing = new Vector2(3, 3);
+        Margin = new Vector2(1, 0.5f);
     }
 
-    void Update()
+    public void Update()
     {
-        //CharacterController controller = GetComponent<CharacterController>();
-        //Camera camera = GetComponent<Camera>();
+        var x = transform.position.x;
+        var y = transform.position.y;
 
-
-        ////Clamp's the velocity value coming from the player's rigidbody betweeen the given values
-        //cameraSizeInput = Mathf.Clamp(Mathf.Abs(controller.velocity.x), 5.0F, 10.0F);
-        ////Using Mathf.SmoothDamp smooth the value from the current camera size to the target size 
-        //cameraSizeChanger = Mathf.SmoothDamp(camera.orthographicSize, cameraSizeInput, ref playerVelocity, smoothTimeCameraSize);
-        ////set the camera's size
-        //camera.orthographicSize = cameraSizeChanger;
-
-        characterY = getCharacterPositionRelativeToCamera(character.transform.position.y, transform.position.y);
-        characterX = getCharacterPositionRelativeToCamera(character.transform.position.x, transform.position.x);
-
-
-        if (characterX >= movementThresholdX || characterY >= movementThresholdY)
+        if (IsFollowing)
         {
-            movementToPosition = character.transform.position;
-            movementToPosition.z = -10; //static z position;
+            if (Mathf.Abs(x - Player.position.x) > Margin.x)
+            {
+                x = Mathf.Lerp(x, Player.position.x, Smoothing.x * Time.deltaTime);
+            }
 
-            //transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
-            //transform.position = Vector3.MoveTowards(transform.position, movementToPosition, speed * Time.deltaTime); //probably need to do movement based on character speed. For now this is a set speed
-            //transform.position = SmoothApproach(transform.position, movementToPosition, 0.5f);
-            var smoothMovementVector = Vector3.SmoothDamp(transform.position, movementToPosition, ref cameraVelocity, smoothTime);
-
-
-            transform.position = smoothMovementVector;
-            //cameraTransform.position = cameraPos;
+            if (Mathf.Abs(y - Player.position.y) > Margin.y)
+            {
+                y = Mathf.Lerp(y, Player.position.y, Smoothing.y * Time.deltaTime);
+            }
         }
+
+        transform.position = new Vector3(x, y, transform.position.z);
     }
 }

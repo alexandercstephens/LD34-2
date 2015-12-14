eec9f9ac3f6967c6b47740cb368d9b8e41b18d60
Alex <alexandercstephens@gmail.com>
2015-12-14 14:14:19 -0600

    added house/tree insides and mushrooms

---
+++ Assets/Scripts/HouseInside.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HouseInside : MonoBehaviour {
+    public Collider2D houseCollider;
+    public Transform player;
+    
+    Collider2D trigger;
+    
+    void Awake ()
+    {
+        trigger = GetComponent<Collider2D>();
+    }
+    
+	void Update ()
+    {
+        if (trigger.OverlapPoint(player.position))
+        {
+            houseCollider.enabled = false;
+            transform.position = new Vector3(transform.position.x, transform.position.y, -0.01f);
+        } else
+        {
+            houseCollider.enabled = true;
+            transform.position = new Vector3(transform.position.x, transform.position.y, 0.01f);
+        }
+	}
+}
